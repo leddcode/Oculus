7cd14319caaa3e31122ad8e5b67ca7695909fda4
leddest <46251307+leddest@users.noreply.github.com>
2022-07-13 14:51:51 +0300

    Improve permutating

--- models/mixins/bucket.py
+++ models/mixins/bucket.py
@@ -3,8 +3,14 @@ from concurrent.futures import ThreadPoolExecutor
 
 class Bucket:
 
-    def __get_key_words(self):
-        return [w for w in self.name.split('.')[:-1] if len(w) > 3]
+    def __get_keywords(self):
+        keywords = [w for w in self.name.split('.')[:-1] if len(w) > 3]
+        if len(keywords) > 1:
+            tmp = keywords[:]
+            for sign in ('', '-', '_'):
+                keywords.extend(
+                    [f'{sign}'.join(tmp), f'{sign}'.join(reversed(tmp))])
+        return keywords
 
     def __add_s3_permutations(self, word, keyword):
         for char in ('-', '_', '.', ''):
@@ -18,7 +24,7 @@ class Bucket:
     def __permutate_s3_urls(self):
         with open(self.CLOUD_LIST, 'r') as wl:
             words = wl.read()
-            for keyword in self.__get_key_words():
+            for keyword in self.__get_keywords():
                 self.permutations.append(
                     f'https://{keyword}.s3.amazonaws.com/')
                 for w in words.splitlines():
@@ -28,10 +34,11 @@ class Bucket:
         with ThreadPoolExecutor(max_workers=self.threads) as executor:
             self.executor = executor
             self.__permutate_s3_urls()
+            print(f' <| Searching for {self.search_type}\n')
             for url in self.permutations:
                 self.futures.append(self.executor.submit(
                     self._make_request, url))
 
     def _search_s3_buckets(self):
-        print(f' <| Searching for {self.search_type}\n')
+        print(f' <| Building permutations\n')
         self.__create_s3_pool()
